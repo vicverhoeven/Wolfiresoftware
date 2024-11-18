8b4c673efe06f55588405ee9cd1f1efab8a40529
Vic-Verhoeven <Vic.verhoeven@student.ucll.be>
2024-11-18 22:34:22 +0100

    testfilereplace

--- Main_page/smallheader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", () => {
-    const header = document.querySelector("header"); 
-    if (window.scrollY > 50) { 
-      header.classList.add("scrolled"); 
-    } else {
-      header.classList.remove("scrolled"); 
-    }
-  });
